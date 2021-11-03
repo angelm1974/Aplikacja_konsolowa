9ce88b36eccdb35de3c34c0fa9fc716d18833dbc
angelm1974 <18185521+angelm1974@users.noreply.github.com>
2021-11-03 12:27:16 +0100

    zmienne przykład

--- Konsolka_1/Program.cs
+++ Konsolka_1/Program.cs
@@ -13,8 +13,20 @@ namespace Konsolka_1
         {
             //Kod wyświetlający napis hello world
             //oraz trzymający konsolę otwartą
+
             Console.WriteLine("Hello World!");
+            zmienne_przyklad1();
             Console.ReadKey();
         }
+
+        /// <summary>
+        /// Sprawdzenie ile pamięci zajmuje dana zmienna
+        /// i wyświetlenie w konsoli
+        /// </summary>
+        static void zmienne_przyklad1()
+        {
+            Console.WriteLine("Moja zmienna typu int zajmuje: {0}", sizeof(int));
+        }
+
     }
 }
