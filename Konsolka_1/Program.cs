49b1d9942ccddd7843bf5cd5f35d9256f33d5bc9
angelm1974 <18185521+angelm1974@users.noreply.github.com>
2021-11-03 12:41:43 +0100

    zmienne referencyjne

--- Konsolka_1/Program.cs
+++ Konsolka_1/Program.cs
@@ -15,7 +15,8 @@ namespace Konsolka_1
             //oraz trzymający konsolę otwartą
 
             Console.WriteLine("Hello World!");
-            zmienne_przyklad1();
+            //zmienne_przyklad1();
+            typ_referencyjny();
             Console.ReadKey();
         }
 
@@ -25,7 +26,17 @@ namespace Konsolka_1
         /// </summary>
         static void zmienne_przyklad1()
         {
-            Console.WriteLine("Moja zmienna typu int zajmuje: {0}", sizeof(int));
+            Console.WriteLine("Moja zmienna typu int zajmuje: {0}", sizeof(char));
+        }
+
+        static void typ_referencyjny()
+        {
+            int olek = 1500;
+
+            object o = olek;
+            olek = 12;
+            int liczba = (int)o;
+            Console.WriteLine("moja liczba to: " + liczba.ToString());
         }
 
     }
