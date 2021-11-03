1da3b247244442436b6e10a23410b37fd4d50fb8
angelm1974 <18185521+angelm1974@users.noreply.github.com>
2021-11-03 12:16:49 +0100

    Dodanie komentarza

--- Konsolka_1/Program.cs
+++ Konsolka_1/Program.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Konsolka_1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Kod wyświetlający napis hello world
             Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
